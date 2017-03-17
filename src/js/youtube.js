2931eae61bc3d5c3fe99ec289c7122c33dfea42f
ItamarGronich <ItamarGronich@Gmail.com>
2017-03-17 16:48:41 +0200

    WIP: Youtube module

--- src/js/youtube.js
+++ src/js/youtube.js
@@ -9,16 +9,28 @@ export default class Youtube {
         .filter(video => video.dataset.videoId);
   }
 
-  init() {
+  generateImages() {
     this.videos.forEach(video => {
       const img = new Image();
       img.addEventListener('load', () => video.appendChild(img));
       img.src = `https://img.youtube.com/vi/${video.dataset.videoId}/sddefault.jpg`
-    })
+    });
+
+    return this;
   }
 
-  loadVideo() {
+  attachListener() {
+    document.addEventListener('click', e => this.loadVideo(this.videos.filter(vid => e.target === vid)));
+    return this;
+  }
 
+  loadVideo(video) {
+    video.forEach(
+      vid =>
+        vid
+          .appendChild(document.createElement('iframe')
+          .setAttribute('src', `${window.location.protocol}://www.youtube.com/embed/${video.dataset[0]}/?wmode=transparent&autoplay=0&theme=dark&controls=1&autohide=0&loop=0&showinfo=0&rel=0&playlist=false&enablejsapi=0`))
+    );
   }
 
   playVideo() {
