510b396c6124888fe48f3f21464cbeab4720a96e
ItamarGronich <ItamarGronich@Gmail.com>
2017-03-18 15:38:53 +0200

    Added youtube component styles

--- src/js/youtube.js
+++ src/js/youtube.js
@@ -6,19 +6,7 @@ export default class Youtube {
    */
   constructor() {
 
-    // Get videos array.
-    this.videos =
-      Array
-        .from(document.querySelectorAll('.Youtube'))
-        .filter(video => video.dataset.videoId);
-
-    // Attaches an event listener to the DOM root.
-    this.attachListener();
-
-    // Generate img thumbs.
-    this.generateImages();
-
-
+    // Connection protocol. HTTP: | HTTPS:
     this.protocol = window.location.protocol;
     this.domain   = 'www.youtube.com/embed';
     this.params   = [
@@ -33,24 +21,32 @@ export default class Youtube {
       'playlist=false',
       'enablejsapi=0'
     ];
+
+    // Get videos array.
+    this.videos =
+      Array
+        .from(document.querySelectorAll('.Youtube'))
+        .filter(video => video.dataset.videoId);
+
+    // Attaches an event listener to the DOM root.
+    this.attachListener();
+
+    // Generate img thumbs.
+    this.generateImages();
   }
 
   /**
    * Generate thumbnails for un-played videos.
+   *
+   * Attach the video thumbnail as background image.
    */
   generateImages() {
-    this.videos.forEach(video => {
-      const img = new Image();
-
-      // Append image when it finished loading.
-      img.addEventListener('load', () => video.appendChild(img));
-      img.src = `${this.protocol}://img.youtube.com/vi/${video.dataset.videoId}/sddefault.jpg`
-    })
+    this.videos.forEach(video => video.style.background = `url(${this.protocol}//img.youtube.com/vi/${video.dataset.videoId}/sddefault.jpg) center/cover no-repeat`)
   }
 
 
   /**
-   * Attaches a listener to the DOM root that fires the load video on videos that have been clicked.
+   * Attaches a listener to the DOM root that fires the loadVideo() function on videos that have been clicked.
    */
   attachListener() {
     document.addEventListener('click', e => this.loadVideo(this.videos.filter(vid => e.target === vid)));
@@ -58,17 +54,17 @@ export default class Youtube {
 
   /**
    * Load the video iframe with all the necessary options.
-   * @param {Element} video - HTML element of that the iframe will be embedded in.
+   * @param {Element[]} videos - HTML element of that the iframe will be embedded in.
    */
-  loadVideo(video) {
-    video.forEach(
+  loadVideo(videos) {
+    videos.forEach(
       vid => {
         const
           iframe   = document.createElement('iframe'),
-          videoId  = iframe.dataset.videoId;
+          videoId  = vid.dataset.videoId;
 
         // Set video source.
-        iframe.setAttribute('src', `${this.protocol}://${this.domain}/${videoId}?${this.params.join('&')}`);
+        iframe.setAttribute('src', `${this.protocol}//${this.domain}/${videoId}?${this.params.join('&')}`);
 
         // Set height and width.
         ['height', 'width'].forEach( att => iframe.setAttribute( att, '100%'));
